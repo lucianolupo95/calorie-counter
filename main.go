c04b00b3c6bf0d0207ad165c8acfba21d6db42f0
LucianoLupo95 <lnlupo95@gmail.com>
2024-10-11 21:23:02 +0200

    Sigo probando cargar variable de entorno

--- main.go
+++ main.go
@@ -4,18 +4,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 
 
 func main() {
-    err := godotenv.Load()
-    fmt.Println("Esta funcionando!")
-    if err != nil {
-        fmt.Println("Error cargando el archivo .env "+ err.Error())
-    }
+    fmt.Println("La variable de entorno es: ")
     fmt.Println(os.Getenv("DB_USER"))
 
 }
