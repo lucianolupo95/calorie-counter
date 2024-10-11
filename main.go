6360be43b3d35979ba3f7198707008411b0cb5d6
LucianoLupo95 <lnlupo95@gmail.com>
2024-10-11 21:33:49 +0200

    Agrego pequeño ruter para probar env.

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -11,7 +13,6 @@ import (
 
 func main() {
     fmt.Println("Esta funcionando!")
-
     dbUser := os.Getenv("DB_USER")
     if dbUser == "" {
         fmt.Println("La variable de entorno DB_USER no está configurada.")
@@ -19,4 +20,11 @@ func main() {
         fmt.Println("DB_USER:", dbUser)
 
     }
+    router := mux.NewRouter()
+    router.HandleFunc("/env", getEnv).Methods("GET")
+}
+
+func getEnv(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("ENV: ")
+    fmt.Println(os.Getenv("DB"))
 }
