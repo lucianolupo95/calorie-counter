9aa7d313acdb76d55b16e24d9c354f7bbe22ccfd
LucianoLupo95 <lnlupo95@gmail.com>
2024-10-11 21:39:12 +0200

    Agrego listenAndServe

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -22,9 +23,12 @@ func main() {
     }
     router := mux.NewRouter()
     router.HandleFunc("/env", getEnv).Methods("GET")
+    log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
+
 }
 
 func getEnv(w http.ResponseWriter, r *http.Request) {
     fmt.Println("ENV: ")
     fmt.Println(os.Getenv("DB"))
+
 }
